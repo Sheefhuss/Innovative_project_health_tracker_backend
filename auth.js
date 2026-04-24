f897e84b5ac2d341e642d382ec5601d726d0ad81
Sheefhuss <sheefahussain029@gmail.com>
2026-04-24 09:23:14 +0530

    Refactor auth.js for cleaner code structure

--- auth.js
+++ auth.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-
-// Register route
 router.post('/register', authController.signup);
-
-// Login route
 router.post('/login', authController.login);
 
 module.exports = router;
